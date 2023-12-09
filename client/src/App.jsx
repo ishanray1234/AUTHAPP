e3f37f8191bbabdf52fc86f9610730d84b1e968c
ishanray1234 <97338953+ishanray1234@users.noreply.github.com>
2023-12-10 01:15:21 +0530

    complete signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 
@@ -12,8 +12,8 @@ export default function App() {
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
-    <Route path="/sign-in" element={<Signin />} />
-    <Route path="/sign-up" element={<Signup />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
     <Route path="/profile" element={<Profile />} />
     </Routes>
   </BrowserRouter>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://[::1]:3000',
+        // changeOrigin: true,
+        secure: false,
+  
+      },
+    },
+  },
   plugins: [react()],
-})
+});
